b0c09194eb6068498106b97b9f78ae35b79fb2ef
Siateus <tecmathe@gmail.com>
2025-01-20 17:14:07 -0300

    update

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -22,6 +22,12 @@ class UsuarioController extends IUsuarioController {
 
   async cadastrarFuncionario(req, res) {
     try {
+      if (!validaCPF(req.body.cpf)) {
+        return res.status(400).json({ message: 'CPF inválido' });
+      }
+      if (!validaEmail(req.body.email)) {
+        return res.status(400).json({ message: 'E-mail inválido' });
+      }
       let user = await usuarioDao.cadastrarFuncionario(req);
       return res.json({ message: 'Funcionário cadastrado com sucesso', user });
     } catch (error) {
@@ -71,6 +77,59 @@ class UsuarioController extends IUsuarioController {
       return res.status(500).json({ message: 'Erro ao atualizar funcionário' });
     }
   }
+}
+function validaEmail(email) {
+  var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+  return regex.test(email);
+}
+function validaCPF(cpf) {
+  var Soma = 0
+  var Resto
+
+  var strCPF = String(cpf).replace(/[^\d]/g, '')
+  
+  if (strCPF.length !== 11)
+     return false
+  
+  if ([
+    '00000000000',
+    '11111111111',
+    '22222222222',
+    '33333333333',
+    '44444444444',
+    '55555555555',
+    '66666666666',
+    '77777777777',
+    '88888888888',
+    '99999999999',
+    ].indexOf(strCPF) !== -1)
+    return false
+
+  for (i=1; i<=9; i++)
+    Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+
+  Resto = (Soma * 10) % 11
+
+  if ((Resto == 10) || (Resto == 11)) 
+    Resto = 0
+
+  if (Resto != parseInt(strCPF.substring(9, 10)) )
+    return false
+
+  Soma = 0
+
+  for (i = 1; i <= 10; i++)
+    Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
+
+  Resto = (Soma * 10) % 11
+
+  if ((Resto == 10) || (Resto == 11)) 
+    Resto = 0
+
+  if (Resto != parseInt(strCPF.substring(10, 11) ) )
+    return false
+
+  return true
 }
 
 module.exports = UsuarioController;
